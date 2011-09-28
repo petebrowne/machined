137f70aa0449f83cd9b6b0860056da3a635d47a6
Pete Browne <me@petebrowne.com>
2011-09-28 12:48:59 -0500

    Adding rack-test gem

--- machined.gemspec
+++ machined.gemspec
@@ -23,5 +23,6 @@ Gem::Specification.new do |s|
   s.add_dependency             "activesupport",  "~> 3.1"
   s.add_dependency             "i18n",           "~> 0.6"
   s.add_development_dependency "rspec",          "~> 2.6"
+  s.add_development_dependency "rack-test",      "~> 0.6"
   s.add_development_dependency "test-construct", "~> 1.2"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,13 @@
 require "bundler"
 Bundler.require :default, :development
 require "test-construct"
+require "rack/test"
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
+  config.include Rack::Test::Methods
   config.include Construct::Helpers
 end
