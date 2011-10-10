a943da159e695471cc7a548ebe411907315a0192
Pete Browne <me@petebrowne.com>
2011-10-10 11:53:02 -0500

    Adding #url method to Context

--- lib/machined/context.rb
+++ lib/machined/context.rb
@@ -33,6 +33,12 @@ module Machined
       machined.config
     end
     
+    # Returns the URL to this asset, appending the sprocket's URL.
+    # For HTML files, this will return pretty URLs.
+    def url
+      File.join(environment.config.url, @logical_path).sub /(index)?\.html$/, ''
+    end
+    
     protected
     
     # Loops through the helpers added to the Machined
--- spec/machined/context_spec.rb
+++ spec/machined/context_spec.rb
@@ -17,4 +17,22 @@ describe Machined::Context do
       end
     end
   end
+  
+  describe "#url" do
+    it "returns the URL for the current page or asset" do
+      within_construct do |c|
+        c.file "assets/javascripts/main.js.erb", "<%= url %>"
+        c.file "assets/stylesheets/main.css.erb", "<%= url %>"
+        c.file "pages/index.html.erb", "<%= url %>"
+        c.file "pages/about.html.erb", "<%= url %>"
+        c.file "pages/about/team.html.erb", "<%= url %>"
+        
+        machined.assets["main.js"].to_s.should == "/assets/main.js;\n"
+        machined.assets["main.css"].to_s.should == "/assets/main.css"
+        machined.pages["index.html"].to_s.should == "/"
+        machined.pages["about.html"].to_s.should == "/about"
+        machined.pages["about/team.html"].to_s.should == "/about/team"
+      end
+    end
+  end
 end
